b5050e55138e3085117b27677f2e781619cead87
timkids <timkids@czr.org>
2025-09-01 14:40:43 +0300

    FEAT: реализация std::formatter для класса bookdb::Book.

--- include/book.hpp
+++ include/book.hpp
@@ -79,6 +79,33 @@ struct formatter<bookdb::Genre, char> {
     }
 };
 
+template <>
+struct formatter<bookdb::Book, char> {
+    template <typename FormatContext>
+    auto format(const bookdb::Book& book, FormatContext &fc) const {
+        std::string genre_str;
+
+        // clang-format off
+        using bookdb::Genre;
+        switch (book.genre) {
+            case Genre::Fiction:    genre_str = "Fiction"; break;
+            case Genre::Mystery:    genre_str = "Mystery"; break;
+            case Genre::NonFiction: genre_str = "NonFiction"; break;
+            case Genre::SciFi:      genre_str = "SciFi"; break;
+            case Genre::Biography:  genre_str = "Biography"; break;
+            case Genre::Unknown:    genre_str = "Unknown"; break;
+            default:
+                throw logic_error{"Unsupported bookdb::Genre"};
+            }
+        // clang-format on
+        return format_to(fc.out(), "{}", genre_str);
+    }
+
+    constexpr auto parse(format_parse_context &ctx) {
+        return ctx.begin();  // Просто игнорируем пользовательский формат
+    }
+};
+
 // Ваш код для std::formatter<Book> здесь
 
 }  // namespace std
