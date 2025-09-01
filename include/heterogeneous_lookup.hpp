0db5db091062c13ad575274ad4981c02890caff7
timkids <timkids@czr.org>
2025-09-01 17:25:28 +0300

    FEAT: реализация гетерогенных операций сравнения для поиска в book_db.

--- include/heterogeneous_lookup.hpp
+++ include/heterogeneous_lookup.hpp
@@ -1,14 +1,40 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
 #include <string_view>
 
 namespace bookdb {
 
-struct TransparentStringLess {};
+struct TransparentStringLess {
+    using is_transparent = void;
 
-struct TransparentStringEqual {};
+    bool operator()(const Book& lhs, std::string_view title) const {
+        return lhs.title < std::string {title};
+    }
+    bool operator()(const Book& lhs, const std::string& author) const {
+        return lhs.author < author;
+    }
+};
 
-struct TransparentStringHash {};
+struct TransparentStringEqual {
+    using is_transparent = void;
+
+    bool operator()(const Book& lhs, std::string_view title) const {
+        return lhs.title == std::string {title};
+    }
+    bool operator()(const Book& lhs, const std::string& author) const {
+        return lhs.author == author;
+    }
+};
+
+struct TransparentStringHash {
+    using is_transparent = void;
+
+    size_t operator()(const Book& book) const {
+        return std::hash<int>()(book.rating) + 37 * std::hash<int>()(book.year) +
+               37 * 37 * std::hash<int>()(book.read_count);
+    }
+};
 
 }  // namespace bookdb
