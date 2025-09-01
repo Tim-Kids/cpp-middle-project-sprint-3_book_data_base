37fba475d3effa67005d070a8340293e2495917f
timkids <timkids@czr.org>
2025-09-01 17:24:27 +0300

    FEAT: частичная реализация концептов.

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -4,22 +4,33 @@
 #include <iterator>
 
 #include "book.hpp"
+#include "comparators.hpp"
 
 namespace bookdb {
 
-template <typename T>
-concept BookContainerLike = true;
+template<typename Cont>
+concept BookContainerLike = requires(Cont c) {
+    typename Cont::value_type;
+    { c.begin() } -> std::same_as<typename Cont::iterator>;
+    { c.end() } -> std::same_as<typename Cont::iterator>;
+    { c.cbegin() } -> std::same_as<typename Cont::const_iterator>;
+    { c.cend() } -> std::same_as<typename Cont::const_iterator>;
+    { c.size() } -> std::same_as<size_t>;
+    { c.empty() } -> std::same_as<bool>;
+};
 
-template <typename T>
-concept BookIterator = true;
+template<typename Iter>
+concept BookIterator =
+    std::forward_iterator<Iter> || std::random_access_iterator<Iter> || std::bidirectional_iterator<Iter>;
 
-template <typename S, typename I>
+template<typename S, typename I>
 concept BookSentinel = true;
 
-template <typename P>
+template<typename P>
 concept BookPredicate = true;
 
-template <typename C>
-concept BookComparator = true;
+template<typename C>
+concept BookComparator = std::same_as<C, comp::LessByYear> || std::same_as<C, comp::LessByAuthor> ||
+                         std::same_as<C, comp::LessByTitle> || std::same_as<C, comp::MoreByRating>;
 
 }  // namespace bookdb
