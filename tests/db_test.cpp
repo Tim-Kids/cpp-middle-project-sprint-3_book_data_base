70f7e9453aee976c3073b1325f205f331abcd6c8
timkids <timkids@czr.org>
2025-09-02 12:55:06 +0300

    TEST: update плана для тестов.

--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -1,3 +1,10 @@
 #include <gtest/gtest.h>
 
-TEST(TestComponentName, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+TEST(TestComponentName, SimpleCheck) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+// 1. Сгенерировать тесты для базового функционала класса BookDatabase (протестировать API: begin, end, size, empty, [],
+// const [], EmplaceBack, GetBooks, GetAuthors, Clear).
+
+// 2. Протестирвоать компараторы с std::sort.
