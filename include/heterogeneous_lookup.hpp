3afce58d2e45e5e6086544a17dd80d023457007f
timkids <timkids@czr.org>
2025-09-02 12:56:23 +0300

    REF: облегчаем гетерогенный функторы.

--- include/heterogeneous_lookup.hpp
+++ include/heterogeneous_lookup.hpp
@@ -9,31 +9,24 @@ namespace bookdb {
 struct TransparentStringLess {
     using is_transparent = void;
 
-    bool operator()(const Book& lhs, std::string_view title) const {
-        return lhs.title < std::string {title};
-    }
-    bool operator()(const Book& lhs, const std::string& author) const {
-        return lhs.author < author;
+    bool operator()(std::string_view lhs, std::string_view rhs) const {
+        return lhs < rhs;
     }
 };
 
 struct TransparentStringEqual {
     using is_transparent = void;
 
-    bool operator()(const Book& lhs, std::string_view title) const {
-        return lhs.title == std::string {title};
-    }
-    bool operator()(const Book& lhs, const std::string& author) const {
-        return lhs.author == author;
+    bool operator()(std::string_view lhs, std::string_view rhs) const {
+        return lhs == rhs;
     }
 };
 
 struct TransparentStringHash {
     using is_transparent = void;
 
-    size_t operator()(const Book& book) const {
-        return std::hash<int>()(book.rating) + 37 * std::hash<int>()(book.year) +
-               37 * 37 * std::hash<int>()(book.read_count);
+    size_t operator()(std::string_view str) const {
+        return std::hash<std::string_view>()(str);
     }
 };
 
