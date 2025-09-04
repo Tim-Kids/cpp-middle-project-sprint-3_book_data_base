f795a1ed44a5f938f87d5787f875bdc38194df8b
timkids <timkids@czr.org>
2025-09-04 12:26:57 +0300

    FEAT: частичная реализация фильтров.

--- include/filters.hpp
+++ include/filters.hpp
@@ -10,19 +10,35 @@ namespace bookdb {
 
 using BookFilter = std::function<bool(const bookdb::Book& book)>;
 
-BookFilter YearBetween = [](const bookdb::Book& book){ return true;};
-BookFilter RatingAbove = [](const bookdb::Book& book){ return true;};
-BookFilter GenreIs = [](const bookdb::Book& book){ return true;};
-
-template<typename... Predicate>
-auto all_of(Predicate... pred) {
-    return (pred() && ...);
+BookFilter YearBetween(short int yearBegin, short int yearEnd) {
+    return [yearBegin, yearEnd](const Book& book) noexcept {
+        return book.year >= std::min(yearBegin, yearEnd) && book.year <= std::max(yearBegin, yearEnd);
+    };
+}
+BookFilter RatingAbove(double rating) {
+    return [rating](const Book& book) noexcept {
+        return book.rating > rating;
+    };
+}
+BookFilter GenreIs(Genre genre) {
+    return [genre](const Book& book) noexcept {
+        return book.genre == genre;
+    };
 }
 
-template<typename... Predicate>
-auto any_of(Predicate... pred) {
-    return (pred() || ...);
+template<BookPredicate... Predicate> auto all_of(Predicate&&... pred) {
+    return [&](const Book& book) noexcept {
+        return (std::forward<Predicate>(pred)(book) && ...);
+    };
 }
 
+template<BookPredicate... Predicate> auto any_of(Predicate... pred) {
+    return [&](const Book& book) noexcept {
+        return (std::forward<Predicate>(pred)(book) || ...);
+    };
+}
 
+//template<BookIterator It, typename Predicate>
+//auto filterBooks(It begin, It end, Predicate pred);
+//    return std::vector<std::reference_wrapper<const Book&>
 }  // namespace bookdb
