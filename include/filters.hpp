cefdd790410f8b81a9a7ae188ae98b0ee64ad47c
timkids <timkids@czr.org>
2025-09-04 22:38:29 +0300

    FIX: устраняем баги с dangling references в лямбдах all_of, any_of.

--- include/filters.hpp
+++ include/filters.hpp
@@ -2,45 +2,65 @@
 
 #include <algorithm>
 #include <functional>
+#include <type_traits>
+#include <iterator>
+#include <vector>
 
 #include "book.hpp"
 #include "concepts.hpp"
 
 namespace bookdb {
 
-using BookFilter = std::function<bool(const bookdb::Book& book)>;
+using BookFilter = std::function<bool(const bookdb::Book&)>;
 
-BookFilter YearBetween(short int yearBegin, short int yearEnd) {
+[[nodiscard]] inline BookFilter YearBetween(short int yearBegin, short int yearEnd) {
     return [yearBegin, yearEnd](const Book& book) noexcept {
         return book.year >= std::min(yearBegin, yearEnd) && book.year <= std::max(yearBegin, yearEnd);
     };
 }
-BookFilter RatingAbove(double rating) {
+
+[[nodiscard]] inline BookFilter RatingAbove(double rating) {
     return [rating](const Book& book) noexcept {
         return book.rating > rating;
     };
 }
-BookFilter GenreIs(Genre genre) {
+
+[[nodiscard]] inline BookFilter GenreIs(Genre genre) {
     return [genre](const Book& book) noexcept {
         return book.genre == genre;
     };
 }
 
-template<BookPredicate... Predicate> auto all_of(Predicate&&... pred) {
-    return [&](const Book& book) noexcept {
-        return (std::forward<Predicate>(pred)(book) && ...);
+template<BookPredicate... Predicate> [[nodiscard]] auto all_of(Predicate&&... pred) {
+    // Передаем предикаты корректно, через std::forward<>. Иначе можно получить dangling reference, если передать как [&].
+    return [...p = std::decay_t<Predicate>(std::forward<Predicate>(pred))](const Book& book) noexcept {
+        return (p(book) && ...);
     };
 }
 
-template<BookPredicate... Predicate> auto any_of(Predicate&&... pred) {
-    return [&](const Book& book) noexcept {
-        return (std::forward<Predicate>(pred)(book) || ...);
+template<BookPredicate... Predicate> [[nodiscard]] auto any_of(Predicate&&... pred) {
+    // Передаем предикаты корректно, через std::forward<>. Иначе можно получить dangling reference, если передать как [&].
+    return [...p = std::decay_t<Predicate>(std::forward<Predicate>(pred))](const Book& book) noexcept {
+        return (p(book) || ...);
     };
 }
 
-template<BookIterator It, typename Predicate> auto filterBooks(It begin, It end, Predicate&& pred) {
-    std::vector<std::reference_wrapper<const Book>> filtered {};
-    std::copy_if(begin, end, std::back_inserter(filtered), std::forward<Predicate>(pred));
-    return filtered;
+template<BookIterator It, BookPredicate... Predicate>
+requires (sizeof...(Predicate) > 0)     // Гарантируем вызов хотя бы с одним предикатом.
+auto filterBooks(It begin, It end, Predicate&&... pred) {
+    return filterBooks(begin, end, all_of(std::forward<Predicate>(pred)...));
 }
-}  // namespace bookdb
+
+template<BookIterator It, BookPredicate Predicate> auto filterBooks(It begin, It end, Predicate&& pred) {
+    using Ref = std::reference_wrapper<const Book>;
+    std::vector<Ref> refs;
+    refs.reserve(std::distance(begin, end));
+    for(auto it = begin; it != end; ++it) {
+        const auto& book = *it;
+        if(pred(book)) {
+            refs.emplace_back(book);
+        }
+    }
+    return refs;
+}
+} // namespace bookdb
